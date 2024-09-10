2411a47841d9b6845c3fb669e90ee430a58d781c
dreammc98 <dreammc98@gmail.com>
2024-09-10 11:26:45 +0700

    loading, error handling

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { Provider } from 'react-redux';
-import { store } from './state/store';
+import App from './app/App';
+import {Provider} from 'react-redux';
+import {store} from './app/store';
 
 const root = ReactDOM.createRoot(
     document.getElementById('root') as HTMLElement
@@ -12,7 +12,7 @@ const root = ReactDOM.createRoot(
 
 root.render(
     <Provider store={store}>
-    <App />
+        <App/>
     </Provider>
 );
 
